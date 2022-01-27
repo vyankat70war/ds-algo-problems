7b295ae0e33e6c0bca31ae3c514e2d5783f0068d
vyankat70war <sattarwar.vyankat@gmail.com>
2022-01-27 21:00:53 +0530

    Added test case

--- src/test/java/com/vss/code/recursion/SwapNodesInPairsTest.java
+++ src/test/java/com/vss/code/recursion/SwapNodesInPairsTest.java
@@ -6,9 +6,18 @@ import org.junit.Test;
 public class SwapNodesInPairsTest{
 
     @Test
-    public void testSwapPairs() {
+    public void testSwapPairs_even() {
         SwapNodesInPairs unit = new SwapNodesInPairs();
         final ListNode head = LinkedListHelper.getList(10).getHead();
+        head.print();
+        final ListNode swappedHead = unit.swapPairs(head);
+        swappedHead.print();
+    }
+
+    @Test
+    public void testSwapPairs_odd() {
+        SwapNodesInPairs unit = new SwapNodesInPairs();
+        final ListNode head = LinkedListHelper.getList(5).getHead();
         head.print();
         final ListNode swappedHead = unit.swapPairs(head);
         swappedHead.print();
