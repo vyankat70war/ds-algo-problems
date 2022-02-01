c9f354c51757e7d6b83b08d16b61a9eee7d5937a
vyankat70war <sattarwar.vyankat@gmail.com>
2022-02-01 21:24:51 +0530

    refactor Pascal Triangle Nth row

--- src/main/java/com/vss/code/recursion/PascalTriangleNthRow.java
+++ src/main/java/com/vss/code/recursion/PascalTriangleNthRow.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangleNthRow {
-    public static List<Integer> getNthRow(int index) {
-        List<Integer> currentRow = new ArrayList<>();
+    public static List<Integer> getNthRow(final int index) {
+        final List<Integer> currentRow = new ArrayList<>();
         currentRow.add(1);
-        if (index == 0) {
-            return currentRow;
+        if (index != 0) {
+            final List<Integer> prevRow = getNthRow(index - 1);
+            for (int i = 1; i < prevRow.size(); i++) {
+                final int current = prevRow.get(i - 1) + prevRow.get(i);
+                currentRow.add(current);
+            }
+            currentRow.add(1);
         }
-
-        List<Integer> prevRow = getNthRow(index - 1);
-
-        for (int i = 1; i < prevRow.size(); i++) {
-            int current = prevRow.get(i - 1) + prevRow.get(i);
-            currentRow.add(current);
-        }
-        currentRow.add(1);
-
         return currentRow;
     }
 }
